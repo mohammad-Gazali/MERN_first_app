7b12d48412339bb9139a5cecdbea4cb5673bce57
MohamadAlgazli <brawjym@gmail.com>
2022-11-22 17:22:00 +0300

    Preparing For Production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors")
@@ -18,6 +19,20 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/goals", require("./routes/goalRoutes"))
 app.use("/api/users", require("./routes/userRoutes"))
+
+
+//* Serve Frontend
+//? This is for production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')))
+} else {
+    app.get("/", (req, res) => {
+        res.send("Please Set To The Site To Production")
+    })
+}
+
 
 //* Middlewares  [Note: These Middlewares must be below routes]
 app.use(errHandler)
